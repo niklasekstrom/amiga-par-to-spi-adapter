e9278dcc7addf30b4e8cc4d39f85d80034ebc7f3
Patrik Axelsson <patrik.bo@gmail.com>
2023-03-05 22:44:22 +0100

    Move version string related values to defines in one file

---
+++ examples/spisd/version.h
@@ -0,0 +1,12 @@
+#ifndef VERSION_H_
+#define VERSION_H_
+
+#define NAME "spisd"
+#define VERSION 2
+#define REVISION 2
+#define DATE "5.3.2023"
+
+extern char device_name[];
+extern char id_string[];
+
+#endif
